bdf9a383e3549169a969637473d2561200ff3d00
Shreyas Jadhav <sshreyasjadhav2021@gmail.com>
2025-01-22 19:28:57 +0530

    Add files via upload

---
+++ a2.java
@@ -0,0 +1,11 @@
+
+class a2
+{
+	public static void main(String args[])
+	{
+		String a[]={"Apple","Banana","Cherry"};
+		
+		for(int i=0;i<a.length;i++)
+			System.out.println("\t"+a[i]+" Length = "+a[i].length());
+	}
+}
