dbe6544fdeeb526363e32b0904e14cdc78ed1a15
Shreyas Jadhav <sshreyasjadhav2021@gmail.com>
2025-02-27 22:49:29 +0530

    Add files via upload

---
+++ Shade.java
@@ -0,0 +1,10 @@
+package Interface;
+
+public abstract class Shade extends Figure{
+	public abstract void draw();
+	public abstract void solidFill();
+	public abstract void edgeFill();
+	public abstract void roundedCorners();
+	
+
+}
